3c7420f5008b79754c49e45f15785f57f80e11e1
Fırat Can Bozkurt <firatcanbozkurt@hotmail.com>
2023-04-02 01:02:52 +0300

    Deployement prepared

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,22 @@
 const functions = require("firebase-functions");
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+exports.chat = functions.https.onRequest(async (request, response) => {
+  const { content } = await request.json();
+
+  const configuration = new Configuration({
+    apiKey: process.env.GPT_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  const completion = await openai
+    .createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: content }],
+    })
+    .then((res) => {
+      return res.data.choices[0];
+    });
+
+  response.send(completion);
 });
